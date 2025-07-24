2330ce39ce31acf0005c8e0f4b652673fd62b884
hwanheejung <hwanheejung117@gmail.com>
2025-07-24 11:47:40 +0900

    ✨ 스텁코드 생성 기능을 구현한다

--- core/stub/index.ts
+++ core/stub/index.ts
@@ -3,6 +3,7 @@ import * as fs from "fs-extra";
 import { GoogleSpreadsheetsContract } from "../google-spreadsheets";
 import { TCSheetBundle } from "./tcsheet-bundle";
 import { Scenario } from "./scenario";
+import { PlaywrightStub } from "./playwright-stub";
 
 type StubGeneratorContract = {
   generateForPlaywright(): Promise<void>;
@@ -27,21 +28,19 @@ class StubGenerator implements StubGeneratorContract {
     try {
       await fs.ensureDir(this.#targetDir);
 
-      const tcSheetBundle = new TCSheetBundle(this.#googleSpreadsheets);
-      const scenario = new Scenario(tcSheetBundle);
-
       // 1. 시트에서 데이터 수집 -> Map<TC-x, TRow[]>
+      const tcSheetBundle = new TCSheetBundle(this.#googleSpreadsheets);
       const rowsMapByPrefix = await tcSheetBundle.collectedRowsMapByPrefix();
 
       // 2. 데이터 구조화 -> Map<TC-x, TScenarioData[]>
+      const scenario = new Scenario(tcSheetBundle);
       const scenariosMapByPrefix = await scenario.scenariosMapByPrefix(
         rowsMapByPrefix
       );
 
-      console.log(">>", scenariosMapByPrefix);
-
-      // 3. 스텁 코드 생성: PlaywrightStubGenerator
-      /// 3.1. TC-x.spec.ts 생성
+      // 3. 스텁 코드 생성
+      const playwrightStub = new PlaywrightStub(scenariosMapByPrefix);
+      await playwrightStub.generate(this.#targetDir);
     } catch (error) {
       throw new Error(`❌ Playwright 스텁 코드 생성 실패: ${error}`);
     }
