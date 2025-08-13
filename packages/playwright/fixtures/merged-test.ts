b750d49d43a18d99cbc72a97d4320094d8a183b4
hwanheejung <hwanheejung117@gmail.com>
2025-08-13 18:23:58 +0900

    🚧 msw/playwright 관련 기능을 우선 주석처리한다

--- packages/playwright/fixtures/merged-test.ts
+++ packages/playwright/fixtures/merged-test.ts
@@ -1,8 +1,9 @@
 import { mergeTests } from "@playwright/test";
 
 import { manualTest } from "./manual-fixture";
-import { mswScenarioTest } from "./msw-scenario-fixture";
 
-const test = mergeTests(mswScenarioTest, manualTest);
+// import { mswScenarioTest } from "./msw-scenario-fixture";
+
+const test = mergeTests(manualTest);
 
 export { test };
