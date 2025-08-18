13aec1b792e78b5f3b99de4447f0df5218ef6ece
hwanheejung <hwanheejung117@gmail.com>
2025-08-18 18:09:58 +0900

    ✨ makeMockSocketServer 함수로 소켓 서버를 생성한다

--- packages/socketio/mock-socket-server/mock-socket-server.ts
+++ packages/socketio/mock-socket-server/mock-socket-server.ts
@@ -1,7 +1,6 @@
 import type { RecordingSourceContract } from "./recording-source-file";
 import type { ScenarioBuilderContract } from "./scenario-builder";
 import type { SocketPlayerContract } from "./socketio-player";
-import type { ScenarioTransformerContract } from "./transformers";
 
 type SocketServerContract = {
   start(): Promise<void>;
@@ -13,21 +12,18 @@ class MockSocketServer implements SocketServerContract {
   readonly #source: RecordingSourceContract;
   readonly #builder: ScenarioBuilderContract;
   readonly #player: SocketPlayerContract;
-  readonly #tfs: ScenarioTransformerContract[];
   readonly #opts?: { speed?: number };
 
-  constructor({ source, builder, player, transformers, buildOptions }: TArgs) {
+  constructor({ source, builder, player, buildOptions }: TArgs) {
     this.#source = source;
     this.#builder = builder;
     this.#player = player;
-    this.#tfs = transformers ?? [];
     this.#opts = buildOptions;
   }
 
   async start(): Promise<void> {
     const rec = await this.#source.load();
-    let s = this.#builder.build(rec, this.#opts);
-    for (const tf of this.#tfs) s = tf(s);
+    const s = this.#builder.build(rec, this.#opts);
 
     await this.#player.start(s);
   }
@@ -47,6 +43,5 @@ type TArgs = {
   source: RecordingSourceContract;
   builder: ScenarioBuilderContract;
   player: SocketPlayerContract;
-  transformers?: ScenarioTransformerContract[];
   buildOptions?: { speed?: number };
 };
